49a7c53951fcb24d79fee3d1705783545adcac04
rharam <raghad.harram@gmail.com>
2023-12-09 15:25:27 +0000

    rr

--- main.go
+++ main.go
@@ -1,151 +1,299 @@
 package main
-
-import (
-	"bufio"
+	
+	import (
 	"fmt"
-	"os"
-	"regexp"
+	
 	"strconv"
+	
+	"regexp"
+	
 	"strings"
-)
-
-func main() {
+	
+	"io/ioutil"
+	
+	"os"
+	)
+	
+	func main() {
 	if len(os.Args) < 2 {
-		fmt.Println("File name missing")
-		return
+	fmt.Println("File name missing")
+	return
 	} else if len(os.Args) > 3 {
-		fmt.Println("Too many arguments")
-		return
+	fmt.Println("Too many arguments")
+	return
+	} else if len(os.Args) != 3 {
+	return
 	}
 	fileName := os.Args[1]
 	file, err := os.ReadFile(fileName)
 	if err != nil {
-		fmt.Println(err.Error())
+	fmt.Println(err.Error())
+	}
+	sample := string(file)
+	if err := ioutil.WriteFile(fileName, []byte(sample), 0644); err != nil {
+	fmt.Println(err.Error())
+	return
+	}
+	err = nonenglishchar(sample)
+	if err != nil {
+	fmt.Println(err)
+	return
+	}
+	
+	regex := regexp.MustCompile(`\s+`)
+	err = brackets(sample)
+	if err != nil {
+	fmt.Println(err)
+	return
 	}
 	words := strings.Split(string(file), " ")
+	if strings.Contains(words[0], "(cap") ||
+	strings.Contains(words[0], "(low") ||
+	strings.Contains(words[0], "(hex") ||
+	strings.Contains(words[0], "(bin") ||
+	strings.Contains(words[0], "(up") ||
+	strings.Contains(words[0], "(low") {
+	fmt.Println("Error: First word is a function")
+	return
+	}
+	
 	for i := 1; i < len(words); i++ {
-		switch words[i] {
-		case "(cap)":
-			words[i-1] = cap(words[i-1])
-			words[i] = ""
-		case "(up)":
-			fmt.Printf("'%s'\n", words[i-1])
-			words[i-1] = up(words[i-1])
-			words[i] = ""
-		case "(low)":
-			words[i-1] = low(words[i-1])
-			words[i] = ""
-		case "(bin)":
-			words[i-1] = bin(words[i-1])
-			words[i] = ""
-		case "(hex)":
-			words[i-1] = hex(words[i-1])
-			words[i] = ""
-		}
+	if strings.Contains(words[i], "(cap)") {
+	if i+1 < len(words) && (strings.Contains(words[i+1], "(cap") || strings.Contains(words[i+1], "(low") || strings.Contains(words[i+1], "(hex") || strings.Contains(words[i+1], "(bin") || strings.Contains(words[i+1], "(up") || strings.Contains(words[i+1], "(low")) {
+	fmt.Println("Error: there is more than one function")
+	return
 	}
-	words = delete_empty(words)
-	regex := regexp.MustCompile(`\s+`)
-	result := regex.ReplaceAllString(strings.Join(words, " "), " $1")
-	result = startsWithVowel(result)
-
-	fileOutput, err := os.Create(os.Args[2])
+	words[i-1] = cap(words[i-1])
+	words[i] = ""
+	}
+	
+	if strings.Contains(words[i], "(up)") {
+	if i+1 < len(words) && (strings.Contains(words[i+1], "(cap") || strings.Contains(words[i+1], "(low") || strings.Contains(words[i+1], "(hex") || strings.Contains(words[i+1], "(bin") || strings.Contains(words[i+1], "(up") || strings.Contains(words[i+1], "(low")) {
+	fmt.Println("Error: there is more than one function")
+	return
+	}
+	words[i-1] = up(words[i-1])
+	words[i] = ""
+	}
+	
+	if strings.Contains(words[i], "(low)") {
+	if i+1 < len(words) && (strings.Contains(words[i+1], "(cap") || strings.Contains(words[i+1], "(low") || strings.Contains(words[i+1], "(hex") || strings.Contains(words[i+1], "(bin") || strings.Contains(words[i+1], "(up") || strings.Contains(words[i+1], "(low")) {
+	fmt.Println("Error: there is more than one function")
+	return
+	}
+	words[i-1] = low(words[i-1])
+	words[i] = ""
+	}
+	
+	if strings.Contains(words[i], "(bin)") {
+	if i+1 < len(words) && (strings.Contains(words[i+1], "(cap") || strings.Contains(words[i+1], "(low") || strings.Contains(words[i+1], "(hex") || strings.Contains(words[i+1], "(bin") || strings.Contains(words[i+1], "(up") || strings.Contains(words[i+1], "(low")) {
+	fmt.Println("Error: there is more than one function")
+	return
+	}
+	words[i-1] = bin(words[i-1])
+	words[i] = ""
+	}
+	
+	if strings.Contains(words[i], "(hex)") {
+	if i+1 < len(words) && (strings.Contains(words[i+1], "(cap") || strings.Contains(words[i+1], "(low") || strings.Contains(words[i+1], "(hex") || strings.Contains(words[i+1], "(bin") || strings.Contains(words[i+1], "(up") || strings.Contains(words[i+1], "(low")) {
+	fmt.Println("Error: there is more than one function")
+	return
+	}
+	words[i-1] = hex(words[i-1])
+	words[i] = ""
+	}
+	
+	if strings.HasPrefix(words[i], "(low,") {
+	if i+2 < len(words) && (strings.Contains(words[i+2], "(cap") || strings.Contains(words[i+2], "(low") || strings.Contains(words[i+2], "(hex") || strings.Contains(words[i+2], "(bin") || strings.Contains(words[i+2], "(up") || strings.Contains(words[i+2], "(low")) {
+	fmt.Println("Error: there is more than one function")
+	return
+	}
+	if i+1 >= len(words) {
+	fmt.Println("Error: No space in between")
+	return
+	}
+	num, err := strconv.Atoi(strings.TrimSuffix(words[i+1][0:], ")"))
 	if err != nil {
-		fmt.Println("Error creating file:", err)
-		return
+	fmt.Println("Error: Invalid Number")
+	return
 	}
-	defer fileOutput.Close()
-
-	w := bufio.NewWriter(fileOutput)
-	_, er := w.WriteString(result)
-	if er != nil {
-		os.Exit(1)
+	if err == nil && i-num >= 0 {
+	startIndex := i - num
+	for j := startIndex; j < i; j++ {
+	words[j] = low(words[j])
 	}
-
-	w.Flush()
-}
-
-func delete_empty(s []string) []string {
-	var r []string
-	for _, str := range s {
-		if str != "" {
-			r = append(r, str)
-		}
+	words[i] = ""
+	words[i+1] = ""
+	} else if i-num < 0 {
+	fmt.Println("Error: No enough words")
+	return
 	}
-	return r
-}
-
-func hex(word string) string {
+	} else if strings.HasPrefix(words[i], "(up,") {
+	if i+2 < len(words) && (strings.Contains(words[i+2], "(cap") || strings.Contains(words[i+2], "(low") || strings.Contains(words[i+2], "(hex") || strings.Contains(words[i+2], "(bin") || strings.Contains(words[i+2], "(up") || strings.Contains(words[i+2], "(low")) {
+	fmt.Println("Error: there is more than one function")
+	return
+	}
+	if i+1 >= len(words) {
+	fmt.Println("Error: No space in between")
+	return
+	}
+	num, err := strconv.Atoi(strings.TrimSuffix(words[i+1][0:], ")"))
+	if err != nil {
+	fmt.Println("Error: Invalid Number")
+	return
+	}
+	if err == nil && i-num >= 0 {
+	startIndex := i - num
+	for j := startIndex; j < i; j++ {
+	words[j] = up(words[j])
+	}
+	words[i] = ""
+	words[i+1] = ""
+	} else if i-num < 0 {
+	fmt.Println("Error: No enough words")
+	return
+	}
+	} else if strings.HasPrefix(words[i], "(cap,") {
+	if i+2 < len(words) && (strings.Contains(words[i+2], "(cap") || strings.Contains(words[i+2], "(low") || strings.Contains(words[i+2], "(hex") || strings.Contains(words[i+2], "(bin") || strings.Contains(words[i+2], "(up") || strings.Contains(words[i+2], "(low")) {
+	fmt.Println("Error: there is more than one function")
+	return
+	}
+	if i+1 >= len(words) {
+	fmt.Println("Error: No space in between")
+	return
+	}
+	num, err := strconv.Atoi(strings.TrimSuffix(words[i+1], ")"))
+	if err != nil {
+	fmt.Println("Error: Invalid Number")
+	return
+	}
+	
+	if err == nil && i-num >= 0 {
+	startIndex := i - num
+	for j := startIndex; j < i; j++ {
+	words[j] = cap(words[j])
+	}
+	words[i] = ""
+	words[i+1] = ""
+	} else if i-num < 0 {
+	fmt.Println("Error: No enough words")
+	return
+	}
+	}
+	}
+	
+	regex = regexp.MustCompile(`\s+`)
+	result := regex.ReplaceAllString(strings.Join(words, " "), " $1")
+	result = punct(result)
+	result = quotes(result)
+	result = doublequotes(result)
+	result = replaceVowelsWithAn(result)
+	output := os.Args[2]
+	if err := ioutil.WriteFile(output, []byte(result), 0644); err != nil {
+	fmt.Println(err.Error())
+	return
+	}
+	
+	}
+	
+	func hex(word string) string {
 	value, err := strconv.ParseInt(word, 16, 64)
 	if err != nil {
-		fmt.Println("Invalid Hex")
-		return ""
+	fmt.Println("Invalid Hex")
+	return ""
 	}
 	return strconv.Itoa(int(value))
-}
-
-func bin(digit string) string {
+	}
+	
+	func bin(digit string) string {
 	binary, err := strconv.ParseInt(digit, 2, 64)
 	if err != nil {
-		fmt.Println("Invalid binary")
-		return ""
+	fmt.Println("Invalid binary")
+	return ""
 	}
 	return strconv.FormatInt(binary, 10)
-}
-
-func up(w string) string {
+	}
+	
+	func up(w string) string {
 	upp := strings.ToUpper(w)
 	return upp
-}
-
-func low(s string) string {
+	}
+	
+	func low(s string) string {
 	loww := strings.ToLower(s)
 	return loww
-}
-
-func cap(maj string) string {
+	}
+	
+	func cap(maj string) string {
 	loww := strings.ToLower(maj)
 	w := strings.Title(loww)
 	return w
-}
-
-//func num(a string) int {
-//  app := func(a rune) bool {
-//  return !unicode.strings(app)
-//}
-//suii :=
-//}
-
-func punct(text string) string {
+	}
+	
+	func punct(text string) string {
 	regex := regexp.MustCompile(`\s*([.,!?;:]+)\s*`)
 	transformed := regex.ReplaceAllString(text, "$1")
-	return transformed
-}
-
-func groupofpunct(text string) string {
-	regex := regexp.MustCompile(`\s*([.,]{2,}|[!?]+)(?:\s+|$)`)
-	transformed := regex.ReplaceAllString(text, "$1 ")
-	return transformed
-}
-
-func punctofquotes(text string) string {
-	regex := regexp.MustCompile(`'\s*(\w+)\s*'`)
-	transformed := regex.ReplaceAllStringFunc(text, func(match string) string {
-		word := strings.Trim(match, "' ")
-		return "'" + word + "'"
-	})
-	return transformed
-}
-
-func morethanoneword(text string) string {
+	re := regexp.MustCompile(`([.,!?;:]+)\s*`)
+	suii := re.ReplaceAllString(transformed, "$1 ")
+	return suii
+	}
+	
+	func quotes(text string) string {
 	regex := regexp.MustCompile(`'\s*(.*?)\s*'`)
 	transformed := regex.ReplaceAllStringFunc(text, func(match string) string {
-		word := strings.Trim(match, "' ")
-		return "'" + word + "'"
+	word := strings.Trim(match, "' ")
+	return "'" + word + "'"
 	})
 	return transformed
-}
-
-func startsWithVowel(t string) string {
-	regex := regexp.MustCompile(`\b([aA])\s+([aeiouh]\w*)`)
-	return regex.ReplaceAllString(t, "${1}n $2")
-}
+	}
+	
+	func doublequotes(text string) string {
+	regex := regexp.MustCompile(`"\s*(.*?)\s*"`)
+	transformed := regex.ReplaceAllStringFunc(text, func(match string) string {
+	word := strings.Trim(match, "\" ")
+	return "\"" + word + "\""
+	})
+	return transformed
+	}
+	
+	func replaceVowelsWithAn(text string) string {
+	words := strings.Fields(text)
+	for i := 0; i < len(words)-1; i++ {
+	word := words[i]
+	nextWord := words[i+1]
+	if (word == "a" || word == "A") && startsWithVowelOrH(nextWord) {
+	if word == "a" {
+	words[i] = "an"
+	} else {
+	words[i] = "An"
+	}
+	}
+	}
+	replaced := strings.Join(words, " ")
+	return replaced
+	}
+	
+	func startsWithVowelOrH(word string) bool {
+	if len(word) == 0 {
+	return false
+	}
+	firstChar := word[:1]
+	return strings.ContainsAny(strings.ToLower(firstChar), "aeiouh")
+	}
+	
+	func nonenglishchar(str string) error {
+	regex := regexp.MustCompile(`[^\x00-\x7F]`)
+	if regex.MatchString(str) {
+	return fmt.Errorf("Error: The string contains non english characters")
+	}
+	return nil
+	}
+	
+	func brackets(x string) error {
+	regex := regexp.MustCompile(`\s+\)`)
+	if regex.MatchString(x) {
+	return fmt.Errorf("Error: There is multiple spaces after the number")
+	}
+	return nil
+	} 
+	
